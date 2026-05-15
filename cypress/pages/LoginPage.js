0b38d24a79a4ca0bddcc6b880bab6bdc58798144
Mohamed Amer <2005-memo-amer@users.noreply.github.com>
2026-05-15 04:42:51 +0300

    fix: separate page objects and test files

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -1,7 +1,19 @@
 class LoginPage {
-  email() { return cy.get('[data-test="email"]'); }
-  password() { return cy.get('[data-test="password"]'); }
-  loginBtn() { return cy.get('[data-test="login-submit"]'); }
-  errorMsg() { return cy.get('[data-test="login-error"]'); }
+  email() {
+    return cy.get('[data-test="email"]');
+  }
+
+  password() {
+    return cy.get('[data-test="password"]');
+  }
+
+  loginBtn() {
+    return cy.get('[data-test="login-submit"]');
+  }
+
+  errorMsg() {
+    return cy.get('[data-test="login-error"]');
+  }
 }
+
 export default new LoginPage();
